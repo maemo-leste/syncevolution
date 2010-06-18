eefba98a806ee0a091a98722cd36021ed26218ec
David Bremner <bremner@unb.ca>
2010-06-17 21:57:07 -0300

    add meego repos

--- debian/git-init-remotes.sh
+++ debian/git-init-remotes.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+git remote add meego git://gitorious.org/meego-middleware/syncevolution.git
 git remote add moblin git://git.moblin.org/syncevolution.git
 git remote add alioth git+ssh://git.debian.org/git/collab-maint/syncevolution.git
